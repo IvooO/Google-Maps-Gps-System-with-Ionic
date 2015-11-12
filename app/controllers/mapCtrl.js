df305c2223ca309028c08d3af4065cadbd5a5b53
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-11-12 17:43:23 -0500

    heroku google maps

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -13,31 +13,31 @@ myapp.controller("mapController", function($scope){
 	$scope.directionsDisplay.setMap($scope.map);
 
 
-	$scope.onChangeHandler = function() {
-    	$scope.calculateAndDisplayRoute($scope.directionsService, $scope.directionsDisplay);
-    	console.log("finding location")
-	};
+	// $scope.onChangeHandler = function() {
+ //    	$scope.calculateAndDisplayRoute($scope.directionsService, $scope.directionsDisplay);
+ //    	console.log("finding location")
+	// };
 
- 	document.getElementById('start').addEventListener('change', $scope.onChangeHandler);
-	document.getElementById('end').addEventListener('change', $scope.onChangeHandler);
+ // 	document.getElementById('start').addEventListener('change', $scope.onChangeHandler);
+	// document.getElementById('end').addEventListener('change', $scope.onChangeHandler);
 
-  $scope.calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
-  	var o = document.getElementById('start').value;
-	var d = document.getElementById('end').value;
-	$scope.orgin = o;
-	$scope.destination = d;
+//   $scope.calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+//   	var o = document.getElementById('start').value;
+// 	var d = document.getElementById('end').value;
+// 	$scope.orgin = o;
+// 	$scope.destination = d;
 
-  	$scope.directionsService.route({
-    	origin: $scope.orgin,
-    	destination: $scope.destination,
-    	travelMode: google.maps.TravelMode.DRIVING
-  }, function(response, status) {
+//   	$scope.directionsService.route({
+//     	origin: $scope.orgin,
+//     	destination: $scope.destination,
+//     	travelMode: google.maps.TravelMode.DRIVING
+//   }, function(response, status) {
   
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-  });
-}
+//     if (status === google.maps.DirectionsStatus.OK) {
+//       directionsDisplay.setDirections(response);
+//     }
+//   });
+// }
 
 
 })
