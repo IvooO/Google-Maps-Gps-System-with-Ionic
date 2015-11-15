cf2caf7f34c14df90857a60a35fbe6b75a8435db
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-11-14 23:18:50 -0500

    testing mobile

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,24 @@
 // 'starter.controllers' is found in controllers.js
 var myapp = angular.module('starter', ['ionic','ngCordova'])
 
+.directive('disableTap', function($timeout) {
+  return {
+    link: function() {
+      $timeout(function() {
+        // Find google places div
+        _.findIndex(angular.element(document.querySelectorAll('.pac-container')), function(container) {
+          // disable ionic data tab
+          container.setAttribute('data-tap-disabled', 'true');
+          // leave input field if google-address-entry is selected
+          container.onclick = function() {
+            document.getElementById('autocomplete').blur();
+          };
+        });
+      },500);
+    }
+  };
+});
+
 myapp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
