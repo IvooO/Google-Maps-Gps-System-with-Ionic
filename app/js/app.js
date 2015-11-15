4fa1a3d68b32e2d86e8dcab1d5ba06b284c2e5b2
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-11-14 23:35:46 -0500

    working auto complete

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,28 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var myapp = angular.module('starter', ['ionic','ngCordova'])
+var myapp = angular.module('starter', ['ionic','ngCordova','ngAutocomplete'])
 
-.directive('disableTap', function($timeout) {
+
+myapp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+
+
+})
+
+myapp.directive('disableTap', function($timeout) {
   return {
     link: function() {
       $timeout(function() {
@@ -24,22 +43,6 @@ var myapp = angular.module('starter', ['ionic','ngCordova'])
     }
   };
 });
-
-myapp.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
 
 myapp.config(function($stateProvider, $urlRouterProvider) {
 
