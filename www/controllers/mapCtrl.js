9a41160ba48dc58b8dc49b932b0c77d39858277d
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-11-17 00:32:12 -0500

    google routing with current location

--- www/controllers/mapCtrl.js
+++ www/controllers/mapCtrl.js
@@ -19,27 +19,19 @@ myapp.controller("mapController", function($scope,$timeout){
 
 
 	var stepDisplay = new google.maps.InfoWindow;
-  	var markerArray = [];
+  var markerArray = [];
 
- 	$scope.start = document.getElementById('start');
+  
  	$scope.end = document.getElementById('end');
 	$scope.searchWrap = document.getElementById('searchWrap')
 
 //auto complete service on input fields  =====================
-	$scope.autocomplete = new google.maps.places.Autocomplete($scope.start);
-	$scope.autocomplete1 = new google.maps.places.Autocomplete($scope.end);
-
+	$scope.autocomplete = new google.maps.places.Autocomplete($scope.end);
 
 //listen to the autocomplete event       =====================
-   	$scope.autocomplete.addListener('place_changed', function() {
-   		console.log("place changed ")
-		$scope.calculateAndDisplayRoute($scope.directionsService, $scope.directionsDisplay, markerArray, stepDisplay, map)   
-  	});
-
-   	$scope.autocomplete1.addListener('place_changed', function() {
-   		console.log("place changed ")
-  		$scope.calculateAndDisplayRoute($scope.directionsService, $scope.directionsDisplay, markerArray, stepDisplay, map)   
-  	});
+  $scope.autocomplete.addListener('place_changed', function() {
+	  $scope.calculateAndDisplayRoute($scope.directionsService, $scope.directionsDisplay, markerArray, stepDisplay, map)   
+  });
 
  	$timeout(function(){
 	var predictionContainer = angular.element(document.getElementsByClassName('pac-container'));
@@ -55,17 +47,30 @@ myapp.controller("mapController", function($scope,$timeout){
    	}
 
 	$scope.calculateAndDisplayRoute = function(directionsService, directionsDisplay,markerArray, stepDisplay, map) {
-		 for (var i = 0; i < markerArray.length; i++) {
-    		markerArray[i].setMap(null);
-  		}
-		orgin = $scope.start.value;
-		destination = $scope.end.value;
-  		directionsService.route({
-	    	origin: orgin,
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+          var newPoint = new google.maps.LatLng(position.coords.latitude, 
+                                                position.coords.longitude);
+
+          $scope.coords = position.coords.latitude + "," + position.coords.longitude
+
+          $scope.newCord = JSON.stringify($scope.coords)
+
+          console.log( $scope.newCord)
+           
+    })
+    for (var i = 0; i < markerArray.length; i++) {
+        markerArray[i].setMap(null);
+      }
+    // console.log($scope.newCord)
+		start = $scope.newCord;
+		destination = $scope.end.value
+  	directionsService.route({
+	    	origin: start,
 	    	destination: destination,
 	    	travelMode: google.maps.TravelMode.DRIVING
 
-	}, function(response, status) {
+	 }, function(response, status) {
 	    if (status === google.maps.DirectionsStatus.OK) {
 	      directionsDisplay.setDirections(response);
 	      showSteps(response, markerArray, stepDisplay, map);
@@ -77,41 +82,62 @@ myapp.controller("mapController", function($scope,$timeout){
 	      $scope.$apply()
 	    }
 	});
-	}
 
+
+
+	}
 
 //define the current position marker =========================
 marker = null;
 
-
-var iconBase = '../img/ionic.png';
-
-console.log(iconBase.src)
-
-//find your location and
-$scope.findLocation = (function(){
+$scope.findLocation = function(){
   navigator.geolocation.getCurrentPosition(function(position) {
     var newPoint = new google.maps.LatLng(position.coords.latitude, 
                                           position.coords.longitude);
+
+
+
+    console.log("position.coords.latitude",position.coords)
     if (marker != null) {
       // Marker already created - Move it
       marker.setPosition(newPoint);
       $scope.map.setCenter(newPoint);
     }else{
-    	marker = new google.maps.Marker({
+      marker = new google.maps.Marker({
+        icon: iconBase,
         position: newPoint,
         map: $scope.map,
+        shape: shape,
         title: 'Current Position'
       });
        $scope.map.setCenter(newPoint);
        marker.addListener('click', toggleBounce);
+      return newPoint;
+
     }
   // Center the map on the new position
   }); 
-  // Call the autoUpdate() function every 1/10 seconds
-  setTimeout($scope.findLocation, 400);
-})()
 
+  // Call the autoUpdate() function every 1/10 seconds
+  // setTimeout($scope.findLocation, 5000);
+
+}
+$scope.findLocation();
+
+
+var iconBase = {
+    url: '../img/arrow.png',
+    size: new google.maps.Size(30, 32),
+     origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
+
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+//find your location and
 function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
